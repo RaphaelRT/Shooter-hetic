8a51c91965230d885975e303799240db0a404368
RaphaelRT <raph.robert974@gmail.com>
2018-11-19 17:45:34 +0100

    :sparkles add bestscore

--- scripts/app.js
+++ scripts/app.js
@@ -8,8 +8,18 @@ shooter.$start = shooter.$container.querySelector('.start')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
+shooter.$bestScore = shooter.$container.querySelector('.best-score .value')
 shooter.score = 0
 shooter.secondsLeft = 0
+shooter.bestScore = window.localStorage.getItem('bestScore')
+
+if(shooter.bestScore === null)
+{
+    shooter.bestScore = 0
+}
+
+shooter.bestScore = parseInt(shooter.bestScore)
+shooter.$bestScore.textContent = shooter.bestScore
 
 shooter.sounds = {}
 shooter.sounds.ding = new Audio('ding.mp3')
@@ -25,23 +35,36 @@ shooter.$start.addEventListener('click', () =>
  */
 shooter.start = () =>
 {
-    shooter.$container.classList.remove('step-end')
     shooter.$container.classList.remove('step-start')
+    shooter.$container.classList.remove('step-end')
     shooter.$container.classList.add('step-game')
-    shooter.secondsLeft = 12
+
+    shooter.secondsLeft = 4
+
     shooter.score = 0
     shooter.$score.textContent = shooter.score
+
     shooter.tick()
 }
 
-shooter.end = () =>{
-  shooter.$container.classList.remove('step-game')
-  shooter.$container.classList.add('step-end')
+shooter.end = () =>
+{
+    shooter.$container.classList.remove('step-game')
+    shooter.$container.classList.add('step-end')
+
+    shooter.sounds.finish.play()
+
+    if(shooter.score > shooter.bestScore)
+    {
+        window.localStorage.setItem('bestScore', shooter.score)
+        shooter.bestScore = shooter.score
+        shooter.$bestScore.textContent = shooter.score
+    }
 }
+
 shooter.tick = () =>
 {
     shooter.secondsLeft--
-    console.log(shooter.secondsLeft)
 
     if(shooter.secondsLeft === 0)
     {
