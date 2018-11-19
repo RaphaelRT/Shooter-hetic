dc8d686c28490f8d2a3815f00bf747ad4eb18f82
RaphaelRT <raph.robert974@gmail.com>
2018-11-19 17:10:18 +0100

    :sparkles: game start

--- scripts/app.js
+++ scripts/app.js
@@ -10,9 +10,20 @@ shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
 shooter.score = 0
 
+shooter.sounds = {}
+shooter.sounds.ding = new Audio('ding.mp3')
+shooter.sounds.finish = new Audio('finish.mp3')
+
+shooter.$start.addEventListener('click', () =>{
+  shooter.start()
+})
 /**
  * Methods
  */
+shooter.start = () =>{
+  shooter.$container.classList.remove('step-start')
+  shooter.$container.classList.add('step-game')
+}
 shooter.addTarget = () =>
 {
     // Create target
@@ -42,6 +53,8 @@ shooter.shootTarget = (_$target) =>
     shooter.$score.textContent = shooter.score
 
     // Play sound
+    shooter.sounds.ding.currentTime = 0
+    shooter.sounds.ding.play()
 }
 
 shooter.addTarget()
