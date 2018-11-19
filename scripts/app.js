0951be02030f368e198f18cf1abfce1b2914115b
RaphaelRT <raph.robert974@gmail.com>
2018-11-19 17:28:05 +0100

    :sparkles: end of the game

--- scripts/app.js
+++ scripts/app.js
@@ -9,21 +9,59 @@ shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
 shooter.score = 0
+shooter.secondsLeft = 0
 
 shooter.sounds = {}
 shooter.sounds.ding = new Audio('ding.mp3')
 shooter.sounds.finish = new Audio('finish.mp3')
 
-shooter.$start.addEventListener('click', () =>{
-  shooter.start()
+shooter.$start.addEventListener('click', () =>
+{
+    shooter.start()
 })
+
 /**
  * Methods
  */
-shooter.start = () =>{
-  shooter.$container.classList.remove('step-start')
-  shooter.$container.classList.add('step-game')
+shooter.start = () =>
+{
+    shooter.$container.classList.remove('step-end')
+    shooter.$container.classList.remove('step-start')
+    shooter.$container.classList.add('step-game')
+    shooter.secondsLeft = 12
+    shooter.score = 0
+    shooter.$score.textContent = shooter.score
+    shooter.tick()
 }
+
+shooter.end = () =>{
+  shooter.$container.classList.remove('step-game')
+  shooter.$container.classList.add('step-end')
+}
+shooter.tick = () =>
+{
+    shooter.secondsLeft--
+    console.log(shooter.secondsLeft)
+
+    if(shooter.secondsLeft === 0)
+    {
+        shooter.end()
+    }
+    else
+    {
+        if(shooter.secondsLeft < 10)
+        {
+            shooter.$timer.textContent = `00:0${shooter.secondsLeft}`
+        }
+        else
+        {
+            shooter.$timer.textContent = `00:${shooter.secondsLeft}`
+        }
+
+        window.setTimeout(shooter.tick, 1000)
+    }
+}
+
 shooter.addTarget = () =>
 {
     // Create target
